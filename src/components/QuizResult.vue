c306ea960d60614188a5199367a70251fce1b4aa
Zach Stone <zachinjapan888@gmail.com>
2022-06-11 16:59:08 -0400

    finish project

---
+++ src/components/QuizResult.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="result">
+    <div class="title">
+      You got {{ totalCorrect }} out of {{ questionsAnswered }}
+    </div>
+    <div class="desc">
+      <div v-if="totalCorrect === questionsAnswered">
+        You got them all correct!
+      </div>
+      <div
+        v-else-if="
+          totalCorrect > questionsAnswered / 2 &&
+          totalCorrect < questionsAnswered
+        "
+      >
+        Not bad!
+      </div>
+      <div v-else>You need to practice more!</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["questionsAnswered", "totalCorrect"],
+};
+</script>
+
+<style></style>
